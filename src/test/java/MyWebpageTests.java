e4eae4573e380aeec7a30526577d7f122ab347a9
Linus Borgenvik <130586388+borlin-2@users.noreply.github.com>
2023-05-05 06:54:57 -0400

    Update MyWebpageTests.java

--- src/test/java/MyWebpageTests.java
+++ src/test/java/MyWebpageTests.java
@@ -1,8 +1,16 @@
+import com.codeborne.selenide.impl.WebElementSelector;
+import io.qameta.allure.internal.shadowed.jackson.databind.JsonNode;
+import io.qameta.allure.internal.shadowed.jackson.databind.ObjectMapper;
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import static com.codeborne.selenide.Selenide.*;
+
+import java.io.File;
 import java.io.IOException;
 
 public class MyWebpageTests {
@@ -24,17 +32,58 @@ public class MyWebpageTests {
         $x("//a[text()='Mitt LTU']").click();
         sleep(2000);
 
-        //Insert the word "Sven"
-        $("input[id='username']").sendKeys("");
+        ChromeDriver driver = new ChromeDriver();
+        // Creating an instance of File with the pathname to the login credentials
+        File jsonFile = new File("C:\\temp\\ltu.json");
 
-        sleep(2000);
+        String email = null;
+        String password = null;
+
+        try {
+            // Creating an instance of ObjectMapper to read the JSON file
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            // Using a FileInputStream to read the JSON file into a JsonNode object
+            JsonNode jsonNode = objectMapper.readTree(jsonFile);
+
+            // Retrieve the username and password from the JsonNode object
+            email = jsonNode.get("ltuCredentials").get("email").asText();
+            password = jsonNode.get("ltuCredentials").get("password").asText();
+
+        } catch (IOException i) {
+            // Print the stack trace of the exception if an error occurs
+            i.printStackTrace();
+        }
+
+        try {
+            // Here we are finding the fields for the email(username) and password, and then we enter the login credentials
+            WebElement emailInput = driver.findElement(By.id("username"));
+            emailInput.sendKeys(email);
+
+            WebElement passwordInput = driver.findElement(By.id("password"));
+            passwordInput.sendKeys(password);
+
+            // Here we click the login button to log in
+            WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+            loginButton.click();
+
+        } catch (Exception e) {
+
+        }
+
+
+
+        //Insert the word "Sven"
+        //$("input[id='username']").sendKeys("");
+
+       // sleep(2000);
 
         //find element by the name "password" and insert the password
-        $("input[name='password']").sendKeys("");
+       // $("input[name='password']").sendKeys("");
 
         //find element by the name "submit" and click it
-        $("input[name='submit']").click();
-        sleep(2000);
+       // $("input[name='submit']").click();
+       // sleep(2000);
         }
 
 
